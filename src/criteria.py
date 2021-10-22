10e7ffcd4eb5d6185db08e0647acba5a0c6013a3
flievano100 <flievano100@misorobotics.com>
2021-10-22 16:17:23 -0500

    video camera calibracion

--- src/criteria.py
+++ src/criteria.py
@@ -7,13 +7,13 @@ import yaml
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((7*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob(r'images/*.jpg')
+images = glob.glob(r'/home/aldemar/Trabajo/tagsAruco/aruco_cv2_multiple_tag_detection/src/images/*.jpg')
 
 #path = 'D:\programacion\aruco_cv2\results'
 #pathlib.Path(path).mkdir(parents=True, exist_ok=True) 
@@ -24,14 +24,14 @@ for fname in images:  # Here, 10 can be changed to whatever number you like to c
     #print(images[im_i])
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,7), None)
+    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)   # Certainly, every loop objp is the same, in 3D.
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,7), corners2, ret)
+        img = cv2.drawChessboardCorners(img, (9,6), corners2, ret)
         found += 1
         cv2.imshow('img', img)
         cv2.waitKey(500)
--- src/main.py
+++ src/main.py
@@ -3,14 +3,18 @@ import cv2
 import cv2.aruco as aruco
 import sys
 import shutil
+import yaml
 
+
+file_='calibration_matrix.yaml'
 #Function to detect tags
 def findArucoMarkers(img, markerSize = 4, totalMarkers=250, draw=True):
+    x=yaml.safe_load(open("src/calibration_matrix.yaml",'r'))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
     arucoDict = aruco.Dictionary_get(key)
     arucoParam = aruco.DetectorParameters_create()
-    bboxs, ids, rejected = aruco.detectMarkers(gray, arucoDict, parameters = arucoParam)
+    bboxs, ids, rejected = aruco.detectMarkers(gray, arucoDict, parameters = arucoParam,cameraMatrix=np.array([[646.7676471528549, 0.0, 318.93014949990015], [0.0, 647.3042988053412, 244.93412329482263], [0.0, 0.0, 1.0]]) ,distCoeff=np.array([[0.04953697129350694, -0.4206829889465348, 0.0017941220198914778, -0.006434768303329634, 1.6399056290328016]]))
     if draw:
         aruco.drawDetectedMarkers(img, bboxs) 
     return [bboxs,ids]
@@ -51,9 +55,9 @@ while True:
     c4=[float(i) for i in [i.split() for i in c4][0]]
    
     cv2.circle(img,(int(sum(c1[0:4])/4),int(sum(c1[4:])/4)),7,(255,0,0),2)
-    cv2.circle(img,(int(sum(c2[0:4])/4),int(sum(c2[4:])/4)),7,(255,0,0),2)
-    cv2.circle(img,(int(sum(c3[0:4])/4),int(sum(c3[4:])/4)),7,(255,0,0),2)
-    cv2.circle(img,(int(sum(c4[0:4])/4),int(sum(c4[4:])/4)),7,(255,0,0),2)
+    cv2.circle(img,(int(sum(c2[0:4])/4),int(sum(c2[4:])/4)),7,(255,255,0),2)
+    cv2.circle(img,(int(sum(c3[0:4])/4),int(sum(c3[4:])/4)),7,(255,255,255),2)
+    cv2.circle(img,(int(sum(c4[0:4])/4),int(sum(c4[4:])/4)),7,(55,55,0),2)
 
     #Array to apply the transformation, the parameters are the previous coordinates that we copy into the c2.txt
 
@@ -61,6 +65,8 @@ while True:
     pt2=np.float32([ [0,0],[480,0],[0,300],[480,300] ])
     tran=cv2.getPerspectiveTransform(pt1,pt2)
     d=cv2.warpPerspective(img,tran,(480,300))
+    
+    z.close()
     
     #Show the new video with apply transformation perspective
     cv2.imshow('img',img)
